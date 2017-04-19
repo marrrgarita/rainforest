9de8a917a9d0d0a8954ef0090ed34f801ce7bd44
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-19 16:10:02 -0400

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
